5b3d0a015878c77bd0aa1f2d476b4f4559f46e8e
Federico Mamoris <fgmamoris>
2020-11-21 14:45:18 -0300

    Configuracion de Usuario model para seguridad

--- models/usuario.js
+++ models/usuario.js
@@ -1,10 +1,53 @@
-var mongoose = require("mongoose");
-var Reserva = require("../models/reserva");
-var Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Reserva = require("../models/reserva");
+const Schema = mongoose.Schema;
+const bcrypt = required("bcrypt");
+const saltRounds = 10; //Da aleatoreadad a la encriptacion
+
+const validateEmail = function (email) {
+  //Regex, expresion regular
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email); //Valida que el string tenga el patron de la expresion regular
+};
 
 var usuarioSchema = new Schema({
-  nombre: String,
+  nombre: {
+    type: String,
+    trim: true,
+    required: [true, "El nombre es obligatorio"], //Tengo que ponerlo ya que el mensaje viene en ingles
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: [true, "El email es obligatorio"], //Tengo que ponerlo ya que el mensaje viene en ingles
+    lowercase: true, //Guarda todo en mminuscula
+    validate: [validateEmail, "Por favor ingrese un email valido"], // validate pertenece a mongoose
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+  },
+  password: {
+    type: String,
+    required: [true, "El password es obligatorio"], //Tengo que ponerlo ya que el mensaje viene en ingles
+  },
+  passwordResetToken: String,
+  passwordResetTokenExpires: Date,
+  verificado: {
+    type: Boolean,
+    default: false,
+  },
 });
+
+//Function pre Antes de guardar(persistir en la bbdd) ejecuta la callback
+usuarioSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hashSync(this.password, saltRounds);
+  }
+  next();
+}); //Sirve para encryptar passowd
+
+//Verifico la veracidad del password
+usuarioSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);//encripto y comparo con el password que tengo en la bbdd
+};
 
 usuarioSchema.methods.reservar = function (biciId, desde, hasta, cb) {
   var reserva = new Reserva({
