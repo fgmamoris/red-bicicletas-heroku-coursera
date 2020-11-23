133a98e5dc3a04fc0872ed5fba51c51b8ffda815
Federico Mamoris <fgmamoris>
2020-11-22 21:37:08 -0300

    Cambio de ruta token

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require("passport"),
   LocalStrategy = require("passport-local").Strategy;
-const Usuario = require("../models/usuario");
+const Usuario = require("./token");
 
 /*Estategia de passport local*/
 passport.use(
